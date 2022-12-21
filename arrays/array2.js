3499143352a9730d3fb57e7e46572768861c98ee
andrew <andrew_mcgrogan@yahoo.com>
2022-12-21 16:21:12 -0500

    fin

--- arrays/array2.js
+++ arrays/array2.js
@@ -13,3 +13,28 @@ const kelly = new dccUser("Looks up to Niel", 101, false);
 const spike = new dccUser("Pool Boy", 69, true);
 
 const dccUserList = [niel, horsey, diply, kelly, spike];
+
+// a) Create a new array consisting only of user's that have an age greater than 20.
+
+// const numberArray20 = dccUserList.filter(Element => Element.age >20);
+// console.log(numberArray20)
+
+// b) Create a new array consisting only of user's that are admins.
+
+// const isAdminOrNot = dccUserList.filter(Element => Element.isAdmin === true);
+// console.log(isAdminOrNot)
+
+// c) Create a new array consisting only of user's that are both admin and have an age less than 50.
+
+// const youngAdmins = dccUserList.filter(Element => Element.isAdmin === true && Element.age >50);
+// console.log (youngAdmins)
+
+// d) Sort the array by the user's names, alphabetically, ascending.
+
+// const alphaAscendingNames = dccUserList.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(alphaAscendingNames)
+
+// e) Sort the array by the user's age, descending (oldest to youngest).
+
+// const oldToYoung = dccUserList.sort((a, b) => b.age - a.age);
+// console.log(oldToYoung)
